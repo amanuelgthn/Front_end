a739f21dc0a572072a7e2a085cb43a70392d22b4
amanuelgthn <amanuelgthn@gmail.com>
2024-05-13 23:18:48 +0200

    Progress

--- React/tictac/src/App.js
+++ React/tictac/src/App.js
@@ -1,4 +1,41 @@
-function Square() {
-  return <button className="square">X</button>
+import { useState } from "react";
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
-export default Square;
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+  return (
+    <>
+        <button 
+              className="square"
+              onClick={handleClick}
+        >
+                {value}
+        </button>
+    </>
+  )
+}
