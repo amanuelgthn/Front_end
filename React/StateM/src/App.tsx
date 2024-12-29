e0c6cc57e28a40b8bae331c6ca64e176505a60ac
amanuelgthn <amanuelgthn@gmail.com>
2024-12-29 23:06:42 +0100

    update

---
+++ React/StateM/src/App.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import { sculptureList } from './data';
+import './App.css'
+
+export default function Gallery() {
+const [index, setIndex ] = useState(0);
+const [showMore, setShowMore] = useState(false)
+const hasNext = index < sculptureList.length -1;
+const hasPrev = index > 0;
+
+function handleNextClick() {
+  if (hasNext) {
+    setIndex(index + 1);
+  }
+}
+function handlePrevClick() {
+  if (hasPrev) {
+    setIndex(index - 1);
+  }
+}
+function handleMoreClick() {
+  setShowMore(!showMore);
+}
+let sculpture = sculptureList[index];
+  return (
+    <>
+    <button onClick={handlePrevClick}>
+      Previous
+    </button>
+    <button onClick={handleNextClick}>
+      Next {index}
+    </button>
+    <h2>
+      <i>{sculpture.name}</i> by {sculpture.artist}
+    </h2>
+    <h3>
+      ({index + 1} of {sculptureList.length})
+    </h3>
+    <button onClick={handleMoreClick}>
+      {showMore ? 'Hide' : 'Show'} details
+    </button>
+    {showMore && <p>{sculpture.description}</p>}
+    <img
+        src={sculpture.url}
+        alt={sculpture.alt}
+    />
+    </>
+  )
+}
+
+
